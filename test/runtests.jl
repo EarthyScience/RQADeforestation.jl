5790cba0a5d0ba133ec4311cb4787cf3b220ca7b
Daniel Loos <dloos@bgc-jena.mpg.de>
2024-12-18 15:41:30 +0100

    Add mock rqatrend

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,24 @@
 using RQADeforestation
 using Test
+using DimensionalData
+using YAXArrays
+using Dates
+using Random
+using Statistics
+
+Random.seed!(1337)
 
 @testset "RQADeforestation.jl" begin
-    # Write your tests here.
+    mock_axes = (
+        Ti(Date("2022-01-01"):Day(1):Date("2022-01-30")),
+        X(range(1, 10, length=10)),
+        Y(range(1, 5, length=15)),
+    )
+    mock_data = rand(30, 10, 15)
+    mock_props = Dict()
+    mock_cube = YAXArray(mock_axes, mock_data, mock_props)
+
+    mock_trend = rqatrend(mock_cube; thresh=0.5)
+    @test mock_trend.axes == (mock_cube.X, mock_cube.Y)
+    @test abs(mean(mock_trend)) < 0.1
 end
