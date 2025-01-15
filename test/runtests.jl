322b23f00aab21d2b017e38d885b8df52fcebee6
Stephan Sahm <Stephan.Sahm@gmx.de>
2025-01-15 11:16:39 +0100

    Rqatrend allocationfree (#45)
    * adding new allocationfree rqatrend function
    
    * ignored tmp folder
    
    * rebase to main got messed up a little
    
    * small fixes
    
    * fix test and warnings
    
    * extra documentation link
    
    * test sysimage fixes linearalgebra to 1.10.7
    
    * github action runs with julia 1.10

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,15 @@
 using RQADeforestation
 using Test
+import AllocCheck
+import Random
+Random.seed!(1234)
 
 @testset "RQADeforestation.jl" begin
     # Write your tests here.
+
+    x = 1:0.01:30
+    y = sin.(x) + 0.1x + rand(length(x))
+
+    @test isapprox(RQADeforestation.rqatrend_impl(y; thresh=0.5), -0.11125611687816017)
+    @test isempty(AllocCheck.check_allocs(RQADeforestation.rqatrend_impl, Tuple{Vector{Float64}}))
 end
