038fecc7fbfcf969cf375d40814120335a19b745
Stephan Sahm <stephan.sahm@jolin.io>
2025-01-17 18:50:39 +0100

    including handling of missing inside rqatrend_impl
    fixes #48
    fixes #44

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,4 +12,14 @@ Random.seed!(1234)
 
     @test isapprox(RQADeforestation.rqatrend_impl(y; thresh=0.5), -0.11125611687816017)
     @test isempty(AllocCheck.check_allocs(RQADeforestation.rqatrend_impl, Tuple{Vector{Float64}}))
+
+
+    y2 = similar(y, Union{Float64, Missing})
+    copy!(y2, y)
+    y2[[1,4,10,20,33,65]] .= missing
+
+
+    @test isapprox(RQADeforestation.rqatrend_impl(y2; thresh=0.5), -0.11069045524336744)
+    @test isempty(AllocCheck.check_allocs(RQADeforestation.rqatrend_impl, Tuple{Vector{Union{Float64,Missing}}}))
+
 end
