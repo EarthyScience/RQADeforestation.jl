e853686858c82511b72865be92d73074c8cc6339
Daniel Loos <dloos@bgc-jena.mpg.de>
2025-01-23 17:00:25 +0100

    Add mock rqatrend (#41) close #35
    * Add mock rqatrend
    
    * Add testdata as artifact (#50)
    
    * add testdata as artifact
    
    * Add test-specific project.toml
    
    * manually install artifacts
    
    * Add mock rqatrend
    
    * Fix duplicate packages in test
    
    * Merge testsets
    
    * Add show difference in test
    
    * Relax test
    
    ---------
    
    Co-authored-by: Fabian Gans <fabiancoding@gmail.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,17 +3,19 @@ using Test
 import AllocCheck
 import Random
 import Pkg: Artifacts.@artifact_str, ensure_artifact_installed
+using DimensionalData
+using YAXArrays
+using Dates
+using Random
+using Statistics
+
 Random.seed!(1234)
 
 ensure_artifact_installed("rqatestdata", "Artifacts.toml")
 testdatapath = joinpath(artifact"rqatestdata", "RQADeforestationTestData-1.0")
 
-@testset "Test data" begin
-    @test isfile(joinpath(testdatapath, "V01R01", "EQUI7_EU020M", "E051N018T3", "SIG0_20210818T051717__VH_D095_E051N018T3_EU020M_V01R01_S1BIWGRDH.tif"))
-end
-
 @testset "RQADeforestation.jl" begin
-    # Write your tests here.
+    @test isfile(joinpath(testdatapath, "V01R01", "EQUI7_EU020M", "E051N018T3", "SIG0_20210818T051717__VH_D095_E051N018T3_EU020M_V01R01_S1BIWGRDH.tif"))
 
     x = 1:0.01:30
     y = sin.(x) + 0.1x + rand(length(x))
@@ -22,12 +24,25 @@ end
     @test isempty(AllocCheck.check_allocs(RQADeforestation.rqatrend_impl, Tuple{Vector{Float64}}))
 
 
-    y2 = similar(y, Union{Float64, Missing})
+    y2 = similar(y, Union{Float64,Missing})
     copy!(y2, y)
-    y2[[1,4,10,20,33,65]] .= missing
+    y2[[1, 4, 10, 20, 33, 65]] .= missing
 
 
     @test isapprox(RQADeforestation.rqatrend_impl(y2; thresh=0.5), -0.11069045524336744)
     @test isempty(AllocCheck.check_allocs(RQADeforestation.rqatrend_impl, Tuple{Vector{Union{Float64,Missing}}}))
 
+    mock_axes = (
+        Ti(Date("2022-01-01"):Day(1):Date("2022-01-30")),
+        X(range(1, 10, length=10)),
+        Y(range(1, 5, length=15)),
+    )
+    mock_data = rand(30, 10, 15)
+    mock_props = Dict()
+    mock_cube = YAXArray(mock_axes, mock_data, mock_props)
+
+    mock_trend = rqatrend(mock_cube; thresh=0.5)
+    @test mock_trend.axes == (mock_cube.X, mock_cube.Y)
+    diff = abs(mean(mock_trend))
+    @test diff < 0.2
 end
