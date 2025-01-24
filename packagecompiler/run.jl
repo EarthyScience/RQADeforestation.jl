a4fcd66332dac4b0dc0fe433f87d95810f78cb5d
Daniel Loos <dloos@bgc-jena.mpg.de>
2025-01-24 15:08:53 +0100

    Fix paths

--- packagecompiler/run.jl
+++ packagecompiler/run.jl
@@ -2,15 +2,14 @@
 
 using PackageCompiler
 
-in_dir = ARGS[1]
-out_dir = ARGS[2]
+dir = ARGS[1]
 
-PackageCompiler.create_app(in_dir, "$out_dir/app";
-    precompile_statements_file="$out_dir/precompile_statements.jl",
+PackageCompiler.create_app(dir, "$dir/packagecompiler/app";
+    precompile_statements_file="$dir/packagecompiler/precompile_statements.jl",
     # see https://github.com/JuliaLang/PackageCompiler.jl/issues/994
     include_lazy_artifacts=true,
     # or try the following, which may work because IntelOpenMP and MKL may actually not be needed
     # include_transitive_dependencies=false,
 )
 # lets have an easy check whether this actually worked
-touch("$out_dir/app/done")
+touch("$dir/packagecompiler/app/done")
