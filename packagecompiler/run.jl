0ee7f8d8d75cb85e12dd52b6a87177d88bfaf2fd
Daniel Loos <dloos@bgc-jena.mpg.de>
2025-01-24 14:21:56 +0100

    Add upload binaries

--- packagecompiler/run.jl
+++ packagecompiler/run.jl
@@ -1,15 +1,16 @@
-#!/usr/bin/env -S julia --project=packagecompiler
+#!/usr/bin/env -S julia --threads=auto
+
 using PackageCompiler
-if isdir("packagecompiler/app")
-    rm("packagecompiler/app.backup", recursive=true, force=true)
-    mv("packagecompiler/app", "packagecompiler/app.backup")
-end
-PackageCompiler.create_app(".", "packagecompiler/app";
-    precompile_statements_file="packagecompiler/precompile_statements.jl",
+
+in_dir = ARGS[1]
+out_dir = ARGS[2]
+
+PackageCompiler.create_app(in_dir, "$out_dir/app";
+    precompile_statements_file="$out_dir/precompile_statements.jl",
     # see https://github.com/JuliaLang/PackageCompiler.jl/issues/994
     include_lazy_artifacts=true,
     # or try the following, which may work because IntelOpenMP and MKL may actually not be needed
     # include_transitive_dependencies=false,
 )
 # lets have an easy check whether this actually worked
-touch("packagecompiler/app/done")
+touch("$out_dir/app/done")
