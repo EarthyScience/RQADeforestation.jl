4814cbe08264abce9b5614a07fe81d2beb1e377e
Fabian Gans <fgans@bgc-jena.mpg.de>
2025-02-12 12:51:25 +0100

    add code for weighted online linear regression

---
+++ src/weightedlinearregression.jl
@@ -0,0 +1,34 @@
+smooth(a, b, γ) = a + γ * (b - a)
+
+struct WeightedFit
+    mx::Float64
+    my::Float64
+    mxx::Float64
+    mxy::Float64
+    mw::Float64
+    n::Int64
+end
+WeightedFit() = WeightedFit(0.0, 0.0, 0.0, 0.0, 0.0, 0)
+function finalizewlinreg(f::WeightedFit)
+    b = (f.mxy - f.mx * f.my) / (f.mxx - f.mx * f.mx)
+    a = f.my - b * f.mx
+    a, b
+end
+@inline function smoowthwlinfit(xi, yi, wi, f::WeightedFit)
+    n = f.n + 1
+    mw = smooth(f.mw, wi, inv(n))
+    updateweight = wi / (mw * n)
+    mx = smooth(f.mx, xi, updateweight)
+    my = smooth(f.my, yi, updateweight)
+    mxx = smooth(f.mxx, xi * xi, updateweight)
+    mxy = smooth(f.mxy, xi * yi, updateweight)
+    return WeightedFit(mx, my, mxx, mxy, mw, n)
+end
+
+function wlinreg(x, y, w)
+    f = WeightedFit()
+    @inbounds for i in eachindex(x, y, w)
+        f = smoowthwlinfit(x[i], y[i], w[i], f)
+    end
+    finalizewlinreg(f)
+end
