6bca6f11e9c8e996aa896f450a0db7bb48c38159
Felix Cremer <felix.cremer@bgc-jena.mpg.de>
2025-02-14 11:49:00 +0100

    Add check for number of significant change pixels on main run
    This is an indicator, that the run is probably correct.
    We might want to also include the checked result of a test run to the testdata folder and then compare against that one

--- test/testdata.jl
+++ test/testdata.jl
@@ -1,6 +1,7 @@
 
 @testitem "testdata julia_main" begin
     import Pkg: Artifacts.@artifact_str, ensure_artifact_installed
+    using YAXArrays, Zarr
 
     ensure_artifact_installed("rqatestdata", "Artifacts.toml")
     testdatapath = joinpath(artifact"rqatestdata", "RQADeforestationTestData-2.0")
@@ -8,7 +9,7 @@
     testdir = "tmp/testdata" 
     rm(testdir, recursive=true, force=true)
     mkpath(testdir)
-    outdir = "$testdir/out.zarr"
+    outdir = "$testdir/out"
     indir = "$testdir/in"
     cp(testdatapath, indir)
 
@@ -18,10 +19,15 @@
         "--continent", "EU",
         "--in-dir", indir,
         "--out-dir", outdir,
+        "--years", "2021"
     ])
     # test normal execution
     RQADeforestation.julia_main()
     # test short cut implementation using cache files
     RQADeforestation.julia_main()
+
+    outpath = joinpath(outdir, "E051N018T3_rqatrend_VH_022_thresh_3.0_year_2021.zarr")
+    c = Cube(outpath)
+    @test count(c .< -1.28) > 200
     copy!(ARGS, OLD_ARGS)
 end
