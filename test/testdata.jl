11c21f0e65060f6950a59bb9f267aa6007b47dcf
Daniel Loos <dloos@bgc-jena.mpg.de>
2025-02-27 10:56:37 +0100

    Fix missing output after calling julia_main

--- test/testdata.jl
+++ test/testdata.jl
@@ -5,25 +5,26 @@
     ensure_artifact_installed("rqatestdata", "Artifacts.toml")
     testdatapath = joinpath(artifact"rqatestdata", "RQADeforestationTestData-2.0")
 
-    testdir = "tmp/testdata"
+    testdir = "/tmp/testdata"
     rm(testdir, recursive=true, force=true)
     mkpath(testdir)
     outdir = "$testdir/out.zarr"
     indir = "$testdir/in"
     cp(testdatapath, indir)
 
-    OLD_ARGS = ARGS[:]
-    copy!(ARGS, [
-        "--tile", "E051N018T3",
-        "--continent", "EU",
-        "--start-date", "2021-01-01",
-        "--end-date", "2021-12-31",
-        "--in-dir", indir,
-        "--out-dir", outdir,
-    ])
-    # test normal execution
-    RQADeforestation.julia_main()
-    # test short cut implementation using cache files
-    RQADeforestation.julia_main()
-    copy!(ARGS, OLD_ARGS)
+    using Zarr
+    using YAXArrays
+    using Dates
+    RQADeforestation.main(;
+        tiles=["E051N018T3"],
+        continent="EU",
+        indir="../../RQADeforestationTestData",
+        start_date=Date("2021-01-01"),
+        end_date=Date("2021-12-31"), outdir=outdir
+    )
+    a = open_dataset(outdir * "/E051N018T3_rqatrend_VH_D022_thresh_3.0.zarr").layer
+
+    @test size(a) == (50, 74)
+    @test minimum(a) < 18
+    @test maximum(a) > 0
 end
