2814e43826fdada1c0462d2ced1113c5bab84d87
Daniel Loos <dloos@bgc-jena.mpg.de>
2025-02-27 11:45:52 +0100

    Re add test julia_main

--- test/testdata.jl
+++ test/testdata.jl
@@ -1,5 +1,5 @@
 
-@testitem "testdata julia_main" begin
+@testitem "testdata main" begin
     import Pkg: Artifacts.@artifact_str, ensure_artifact_installed
 
     ensure_artifact_installed("rqatestdata", "Artifacts.toml")
@@ -28,4 +28,35 @@
     @test size(a) == (50, 74)
     @test minimum(a) < 0
     @test maximum(a) > 0
+end
+
+@testitem "testdata julia_main" begin
+    import Pkg: Artifacts.@artifact_str, ensure_artifact_installed
+
+    ensure_artifact_installed("rqatestdata", "Artifacts.toml")
+    testdatapath = joinpath(artifact"rqatestdata", "RQADeforestationTestData-2.0")
+
+    testdir = "tmp/testdata"
+    rm(testdir, recursive=true, force=true)
+    mkpath(testdir)
+    outdir = "$testdir/out.zarr"
+    indir = "$testdir/in"
+    cp(testdatapath, indir)
+
+    OLD_ARGS = ARGS[:]
+    copy!(ARGS, [
+        "--tile", "E051N018T3",
+        "--continent", "EU",
+        "--start-date", "2021-01-01",
+        "--end-date", "2022-01-01",
+        "--in-dir", indir,
+        "--out-dir", outdir,
+    ])
+    # test normal execution
+    RQADeforestation.julia_main()
+    # test short cut implementation using cache files
+    RQADeforestation.julia_main()
+    copy!(ARGS, OLD_ARGS)
+
+    @test outdir |> readdir |> length > 1
 end
